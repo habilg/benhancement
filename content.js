1206fb100ee0fa18df7ceca454cf64425ef4776e
unknown <habil1391@gmail.com>
2020-09-01 16:36:21 +0430

    content revised

--- content.js
+++ content.js
@@ -9,7 +9,7 @@ fundamentalPanel.style.height = "1000px"
 fundamentalPanel.style.backgroundColor = "yellow"
 fundamentalPanel.style.float = "right"
 var target = document.querySelector("#form1")
-target.insertAdjacentElement("beforeend",fundamentalPanel)
+target.insertAdjacentElement("beforeend", fundamentalPanel)
 
 
 var widgetPanel = document.createElement('div')
@@ -18,7 +18,7 @@ widgetPanel.style.height = "1000px"
 widgetPanel.style.backgroundColor = "red"
 widgetPanel.style.float = "left"
 var target = document.querySelector("#form1")
-target.insertAdjacentElement("beforeend",widgetPanel)
+target.insertAdjacentElement("beforeend", widgetPanel)
 
 
 
@@ -30,21 +30,22 @@ target.insertAdjacentElement("beforeend",widgetPanel)
 //3- buyersPower chart
 
 var buyerPower = () => {
-    return (parseFloat(document.querySelector("#e0").textContent) * parseFloat(document.querySelector("#e8").textContent)) / (parseFloat(document.querySelector("#e3").textContent) *parseFloat( document.querySelector("#e5").textContent)).toFixed(2)
+    return (parseFloat(document.querySelector("#e0").textContent) * parseFloat(document.querySelector("#e8").textContent)) / (parseFloat(document.querySelector("#e3").textContent) * parseFloat(document.querySelector("#e5").textContent)).toFixed(2)
 }
 
 var corpSupply = () => {
     var corpSellPortion = parseFloat(document.querySelector("#e4").textContent) / (parseFloat(document.querySelector("#e4").textContent) + parseFloat(document.querySelector("#e3").textContent))
     var realBuyPortion = parseFloat(document.querySelector("#e0").textContent) / (parseFloat(document.querySelector("#e0").textContent) + parseFloat(document.querySelector("#e1").textContent))
-    return (corpSellPortion*realBuyPortion*100).toFixed(2)
+    return (corpSellPortion * realBuyPortion * 100).toFixed(2)
 }
 
 var dealBasePortion = () => {
-    return document.querySelector("#d09 > div").textContent / document.querySelector("#TopBox > div.box2.zi1 > div:nth-child(4) > table > tbody > tr:nth-child(2) > td:nth-child(2) > div").textContent
+
+    return (parseInt(document.querySelector("#d09 > div").getAttribute('title')) / parseInt(document.querySelector("#TopBox > div.box2.zi1 > div:nth-child(4) > table > tbody > tr:nth-child(2) > td:nth-child(2) > div").getAttribute('title'))).toFixed(2)
 }
 
 var dealToMean = () => {
-    return document.querySelector("#d09 > div").textContent / document.querySelector("#TopBox > div.box2.zi1 > div:nth-child(4) > table > tbody > tr:nth-child(4) > td:nth-child(2) > div").textContent
+    return (parseInt(document.querySelector("#d09 > div").getAttribute('title')) / parseInt(document.querySelector("#TopBox > div.box2.zi1 > div:nth-child(4) > table > tbody > tr:nth-child(4) > td:nth-child(2) > div").getAttribute('title'))).toFixed(2)
 }
 
 
@@ -59,38 +60,44 @@ var changeStyleOfBox6 = () => {
 }
 
 //convert table to chart
-var convertBox6toChart = ()=>{
-    let xAxis={
+var convertBox6toChart = () => {
+
+    let chartElement = document.createElement('div')
+    chartElement.id = "chartarea"
+    var target = document.querySelector('.box2.zi2 .box6')
+    target.insertAdjacentElement("beforeend", chartElement)
+
+    let xAxis = {
         visble: false
     }
-    let yAxis ={
+    let yAxis = {
         visible: false
     }
-    let chart={
-        renederto:'??'
+    let chart = {
+        // renederto:'??'
     }
-    let tooltip={
+    let tooltip = {
         enabled: false
     }
-    let legend={
+    let legend = {
         enabled: false
     }
-     let credits={
-         enabled: false
-     }
-     let series=[
+    let credits = {
+        enabled: false
+    }
+    let series = [
         {
             name: 'real',
             data: [
-                {y:`${document.getElementById("#e0").textContent}`,color: 'green'}, //خرید حقیقی
-                {y:`${document.getElementById("#e3").textContent}`,color: '#FF0000'}] //فروش حقیقی
+                { y: `${parseFloat(document.querySelector("#e0 > div:nth-child(2)").getAttribute('title'))}`, color: 'green' }, //خرید حقیقی
+                { y: `${parseFloat(document.querySelector("#e3 > div:nth-child(2)").getAttribute('title'))}`, color: '#FF0000' }] //فروش حقیقی
         }
         ,
         {
             name: 'corp',
             data: [
-                {y:`${document.getElementById("#e1").textContent}`,color: 'green'}, //خرید حقوقی
-                {y:`${document.getElementById("#e4").textContent}`,color: '#FF0000'}] // فروش حقوقی
+                { y: `${parseFloat(document.querySelector("#e1 > div:nth-child(2)").getAttribute('title'))}`, color: 'green' }, //خرید حقوقی
+                { y: `${parseFloat(document.querySelector("#e4 > div:nth-child(2)").getAttribute('title'))}`, color: '#FF0000' }] // فروش حقوقی
         }
     ]
     let plotOptions = {
@@ -108,8 +115,8 @@ var convertBox6toChart = ()=>{
     };
     let json = {};
 
-    json.title = title;
-    json.subtitle = subtitle;
+    // json.title = title;
+    // json.subtitle = subtitle;
     json.xAxis = xAxis;
     json.yAxis = yAxis;
     json.tooltip = tooltip;
@@ -119,8 +126,9 @@ var convertBox6toChart = ()=>{
     json.credits = credits;
     json.chart = chart
 
-    Highcahrts.chart('container',json)
+    Highcharts.chart('chartarea', json)
 }
+
 
 
 
@@ -129,25 +137,25 @@ var convertBox6toChart = ()=>{
 // monitor changes in buy que volume
 var volQueObserver = () => {
     var point = {
-        s:0,
-        v:0,
-        b:0,
+        s: 0,
+        v: 0,
+        b: 0,
     }
 
     var scope = document.querySelector("#bl > tr:nth-child(2) > td:nth-child(2)")// buyers que
     var observer = new MutationObserver(mutationRecord => {
-        point.b=mutationRecord //?
+        point.b = mutationRecord //?
         point.s = document.querySelector("#bl > tr:nth-child(2) > td:nth-child(5)").textContent;
         point.v = document.querySelector("#d09 > div").getAttribute('title');
         updateQVplot(point)
     })
     observer.observe(scope, {
-        childList:true
+        childList: true
     })
 }
 
 
-var updateQVplot = (point)=>{
+var updateQVplot = (point) => {
     var points = [];
     points.push(point);
     //setup highcharts and draw ...
